42547501355f94179174dd2dc180e7ab27dedfb1
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-08-30 21:32:36 -0300

    primeiro exercicio

---
+++ exercicio-001/main.c
@@ -0,0 +1,13 @@
+// escreva um programa que vai apresentar na tela as seguintes informaçoes; 
+
+// o teu nome
+// na linha de baixo o da sua mae
+// na linha de baixo o da sua avo
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Gabriel\nMaisa\nFrancisca");
+    return 0;
+}
